c5be715e1e9caa8d7c30438e0fd1af4375af5992
Georg <georg.strassberger@googlemail.com>
2022-09-24 20:00:08 +0200

    add design and coutdown

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-addEventListener('DOMContentLoaded');
+// Set start date
+const startDate = new Date(2022, 9, 5, 7, 0, 0);
+
+//
+let countdown = setInterval(showTime, 1000);
+
+function showTime() {
+    // Current Date
+    let currentDate = new Date();
+
+    // Time difference between the current Date and the start Date
+    let timeDifference = startDate - currentDate;
+
+    //Convert the single time parts 
+    let dayCount = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    let hourCount = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minuteCount = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    let secondCount = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+    let days = document.getElementById('days');
+    let houre = document.getElementById('houre');
+    let minutes = document.getElementById('minutes');
+    let seconds = document.getElementById('seconds');
+    days.innerHTML = dayCount;
+    houre.innerHTML = hourCount;
+    minutes.innerHTML = minuteCount;
+    seconds.innerHTML = secondCount;
+
+    if (timeDifference < 0) {
+        clearInterval(countdown);
+        const timer = document.getElementById('timer-box');
+        timer.classList.add('d-none');
+        let message = document.getElementById('message');
+        message.innerHTML = 'Los gehts!';
+    }
+}
